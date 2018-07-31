fb72d22b0a004e99f85b6f06e0a196fccf0aadcd
andreas virkus <andreas.virkus@bigbank.ee>
2018-07-31 22:12:58 +0300

    :tada: Initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="home">
+    <form name="reg" method="post">
+      <input type="hidden" name="form-name" value="reg" />
+      <p style="display:none;">
+        <label>Don’t fill this out: <input name="bot-field"></label>
+      </p>
+
+      <div class="form-control">
+        <label>Name</label>
+        <input type="text" name="name" placeholder="John Smith">
+      </div>
+
+      <div class="form-control-group">
+        <div class="form-control">
+          <label>Class</label>
+          <input type="text" name="class" placeholder="Junior/Adult">
+        </div>
+
+        <div class="form-control grow-2x">
+          <label>E-mail</label>
+          <input type="email" name="email" placeholder="mail@domain.com">
+        </div>
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+const encode = (data) => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  methods: {
+    submit (e) {
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: encode({ 'form-name': 'contact', ...this.data })
+      }).then(() => alert('Success!'))
+        .catch(error => alert(error))
+    }
   }
 }
 </script>
+
+<style>
+  main {
+    max-width: 30rem;
+    padding: 1rem;
+    width: 90%;
+    margin: 0 auto;
+  }
+
+  form > * {
+    margin-top: 3rem;
+  }
+
+  input {
+    display: block;
+    appearance: none;
+    width: 100%;
+    padding: 10px;
+    line-height: 20px;
+    font-size: 16px;
+    font-weight: inherit;
+    background: #fff;
+    border-radius: 5px;
+    border: 1px solid #bdbdbd;
+  }
+</style>
