e4fe210dc94d26436c73aaf6572f6beea371bfe5
andreasvirkus <andreasvirkus@gmail.com>
2019-07-28 18:08:34 +0300

    Change submit page copy

--- src/views/Submit.vue
+++ src/views/Submit.vue
@@ -1,6 +1,20 @@
 <template>
   <main class="submit">
-    <h1>Form submitted, thanks!</h1>
-    <router-link to="/reg">👈 Back</router-link>
+    <h1>Registered!</h1>
+    <p>You can currently pay via a bank transfer
+      <a href="https://www.wfac2020.org/entrance-fee"
+        target="_blank"
+        rel="noopener noreferrer">here</a>.
+    </p>
+    <p>As soon as new card payment options open up, we'll notify you via e-mail.</p>
+    <router-link to="/reg">👈 Register another person</router-link>
   </main>
 </template>
+
+<style>
+main.submit {
+  max-width: 30rem;
+  margin: 1rem auto;
+  font-size: 1.6rem;
+}
+</style>
