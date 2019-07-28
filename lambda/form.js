42b8d6ce0d7eac89e610a87c011c84db4fccf0d1
andreasvirkus <andreasvirkus@gmail.com>
2019-07-28 20:43:46 +0300

    Add necessary form flag for got package

--- lambda/form.js
+++ lambda/form.js
@@ -3,14 +3,13 @@ const got = require('got')
 exports.handler = async (event, context, callback) => {
   // Budget honeypot
   console.log('Event body:', event.body)
-  const body = JSON.parse(event.body)
+  // const body = JSON.parse(event.body)
+  const body = event.body
   const { formName, check } = body
   console.log('parsed', formName, check)
   if (formName !== 'reg' || check !== 'must-reg') {
     return callback(null, { statusCode: 403 })
   }
-
-  console.log('Body received:', event.body)
 
   // Post to Google Sheets
   const res = got(process.env.FORM_WEBHOOK_URL, {
