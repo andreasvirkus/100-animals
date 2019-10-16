1ba2c87a395d6a67a547677b8a23c73f5ac0d350
ants <andreasvirkus@gmail.com>
2019-10-16 11:16:22 +0300

    Enable accommodation and update room quantity

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -114,7 +114,7 @@
         </multiselect>
       </div>
 
-      <div v-if="false" class="form-control">
+      <div class="form-control">
         <label for="accommodation">Accommodation</label>
         <checkbox v-model="wantsAccommodation" name="accommodation"/>
       </div>
