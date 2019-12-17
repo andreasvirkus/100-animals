96f7f21038e1ac3580554e247064f24345f53a0a
ants <andreasvirkus@gmail.com>
2019-12-17 13:36:54 +0200

    Show registration form

--- src/router.js
+++ src/router.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 // import Home from './views/Home.vue'
-// import Register from './views/Register.vue'
-import Closed from './views/Closed.vue'
+import Register from './views/Register.vue'
+// import Closed from './views/Closed.vue'
 
 Vue.use(Router)
 
@@ -13,7 +13,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Closed // Register // Home
+      component: Register // Home
     },
     {
       path: '/about',
