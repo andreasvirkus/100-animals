d58391873dc6334457a64fe430c1fb782e766c08
andreasvirkus <andreasvirkus@gmail.com>
2021-05-07 13:38:50 +0300

    Fix router import

---
+++ src/views/Submit.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="submit">
+    <h1>Registration confirmed!</h1>
+    <p>Thank you for registering.</p>
+    <a href="javascript:history.back()">
+      👈 Register another person
+    </a>
+  </main>
+</template>
+
+<style>
+main.submit {
+  max-width: 30rem;
+  margin: 1rem auto;
+  font-size: 1.6rem;
+}
+</style>
